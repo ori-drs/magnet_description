efe8e3d40a520fef93a5249f30589dcac9cae657
Tobit Flatscher <53856473+2b-t@users.noreply.github.com>
2025-03-19 19:15:09 +0000

    fix: Remove nodes for Gazebo Ignition

--- launch/visualize.launch.py
+++ launch/visualize.launch.py
@@ -13,7 +13,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import AndSubstitution, LaunchConfiguration, NotSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
@@ -21,20 +21,11 @@ def generate_launch_description():
     simulation_parameter_name = 'simulation'
     simulation = LaunchConfiguration(simulation_parameter_name)
 
-    #gazebo_ign_parameter_name = 'gazebo_ign'
-    #gazebo_ign = LaunchConfiguration(gazebo_ign_parameter_name)
-
     simulation_parameter_arg = DeclareLaunchArgument(
         simulation_parameter_name,
         default_value='true',
         description='Simulated or real hardware interface'
     )
-
-    #gazebo_ign_arg = DeclareLaunchArgument(
-    #    gazebo_ign_parameter_name,
-    #    default_value='true',
-    #    description='Use Gazebo Ignition or alternatively Gazebo Classic'
-    #)
 
     description_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -60,42 +51,7 @@ def generate_launch_description():
         name='rviz2',
         arguments=['--display-config', rviz_file]
     )
-
-    #is_gazebo_ign = AndSubstitution(simulation, gazebo_ign)
-    #is_gazebo_classic = AndSubstitution(simulation, NotSubstitution(gazebo_ign))
-
-    # Gazebo Ignition (recommended)
-    """
-    gazebo_ign_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                os.path.join(
-                    get_package_share_directory('ros_gz_sim'),
-                    'launch', 'gz_sim.launch.py'
-                )
-            ]
-        ),
-        launch_arguments={
-            'gz_args': " -r -v 3 empty.sdf"
-        }.items(),
-        condition=IfCondition(is_gazebo_ign)
-    )
-    gazebo_ign_spawner_node = Node(
-        package="ros_gz_sim",
-        executable="create",
-        output="screen",
-        arguments=[
-            "-topic",
-            "/robot_description",
-            "-name",
-            "magnet",
-            "-allow_renaming",
-            "true",
-        ]
-    )
-    """
     
-    # Gazebo Classic (deprecated)
     gazebo_classic_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
@@ -118,11 +74,8 @@ def generate_launch_description():
 
     ld = LaunchDescription()
     ld.add_action(simulation_parameter_arg)
-    #ld.add_action(gazebo_ign_arg)
     ld.add_action(description_launch)
     ld.add_action(rviz_node)
-    #ld.add_action(gazebo_ign_node)
-    #ld.add_action(gazebo_ign_spawner_node)
     ld.add_action(gazebo_classic_node)
     ld.add_action(gazebo_classic_spawner_node)
     return ld
